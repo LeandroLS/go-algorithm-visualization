019b59b8439fe5e87690f2495e8695945fa246ea
Leandro Lima <llimas@outlook.com>
2022-01-06 22:35:36 -0300

    tryng to display in order

--- go-algorithm-visualization.go
+++ go-algorithm-visualization.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"math"
 	"math/rand"
 
 	"github.com/oakmound/oak/v3"
@@ -34,6 +35,35 @@ func selectionSort(intSlice []int) []int {
 	return sortedSlice
 }
 
+func searchMinIndexCB(CBSlice []*render.Sprite) int {
+	min := CBSlice[0].GetRGBA().Rect.Min.Y
+	minIndex := 0
+	for i, v := range CBSlice {
+		if math.Abs(float64(v.GetRGBA().Rect.Min.Y)) < math.Abs(float64(min)) {
+			min = CBSlice[i].GetRGBA().Rect.Min.Y
+			minIndex = i
+		}
+	}
+	return minIndex
+}
+
+func selectionSortCB(CBSlice []*render.Sprite) []*render.Sprite {
+	sortedSlice := []*render.Sprite{}
+
+	for _, _ = range CBSlice {
+		minIndex := searchMinIndexCB(CBSlice)
+		colorBox := CBSlice[minIndex]
+		// fmt.Printf("X: %+v\n", colorBox.Vector.X())
+		// fmt.Printf("Y: %+v\n", colorBox.Vector.Y())
+		// fmt.Printf("X RGBA: %+v\n", colorBox.GetRGBA().Rect.Min.X)
+		// fmt.Printf("Y RGBA: %+v\n", colorBox.GetRGBA().Rect.Min.Y)
+		CBSlice = append(CBSlice[:minIndex], CBSlice[minIndex+1:]...)
+		sortedSlice = append(sortedSlice, colorBox)
+	}
+	fmt.Println("Tamanho", len(sortedSlice))
+	return sortedSlice
+}
+
 const qtdColorBox = 100
 
 func makeColorBoxes(qtd int) []*render.Sprite {
@@ -59,8 +89,21 @@ func main() {
 		Start: func(ctx *scene.Context) {
 			fmt.Println("Start scene 1")
 			colorBoxes := makeColorBoxes(qtdColorBox)
-			for i := 0; i < len(colorBoxes); i++ {
-				drawColorBoxes(ctx, colorBoxes[i])
+			// for i := 0; i < len(colorBoxes); i++ {
+			// 	fmt.Printf("X: %+v\n", colorBoxes[i].Vector.X())
+			// 	fmt.Printf("Y: %+v\n", colorBoxes[i].Vector.Y())
+			// 	fmt.Printf("X RGBA: %+v\n", colorBoxes[i].GetRGBA().Rect.Min.X)
+			// 	fmt.Printf("Y RGBA: %+v\n", colorBoxes[i].GetRGBA().Rect.Min.Y)
+			// }
+			cbs := selectionSortCB(colorBoxes)
+			for i := 0; i < len(cbs); i++ {
+				fmt.Printf("X: %+v\n", cbs[i].Vector.X())
+				fmt.Printf("Y: %+v\n", cbs[i].Vector.Y())
+				fmt.Printf("X RGBA: %+v\n", cbs[i].GetRGBA().Rect.Min.X)
+				fmt.Printf("Y RGBA: %+v\n", cbs[i].GetRGBA().Rect.Min.Y)
+			}
+			for i := 0; i < len(cbs); i++ {
+				drawColorBoxes(ctx, cbs[i])
 			}
 			dFPS := render.NewDrawFPS(0.1, nil, 600, 10)
 			ctx.DrawStack.Draw(dFPS, 1)
